bfdc3670544cf32707728539c15537266faa95e2
“Gabriel <bielchristo@hotmail.com>
2021-03-31 19:29:20 -0400

    implemented bcrypt for login and signup
    Co-authored-by: annieshinn <annieshinn@users.noreply.github.com>

--- server/authController.ts
+++ server/authController.ts
@@ -7,6 +7,8 @@
 
 import express, { Request, Response, NextFunction, Errback } from "express";
 const db = require('./models/dataModels.ts')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 const authController = {
   login: function(req: Request, res: Response, next: NextFunction): any { },
@@ -18,24 +20,30 @@ authController.login = (req, res, next) => {
   const checkUser = 'SELECT username, password FROM users WHERE username = $1';
 
   db.query(checkUser, [username])
-    .then((data: any) => {
+    .then(async (data: any) => {
       // username doesn't exist
       if (data.rows.length === 0) {
         res.locals.message = 'username does not exist.';
         return next();
       }
 
+      const hashedPassword = data.rows[0].password
+      const match = await bcrypt.compare(password, hashedPassword)
+
       // check that password is wrong
-      if (data.rows[0].password !== password) {
+      if (!match) {
         res.locals.message = 'wrong password.';
         return next();
       }
 
       // check that password is correct
-      if (data.rows[0].password === password) {
+      if (match) {
         res.locals.message = 'logged in!';
         return next();
       }
+
+      res.locals.message = 'something went wrong.'
+      return next();
     })
     .catch((err: any) => {
       return next({
@@ -46,10 +54,12 @@ authController.login = (req, res, next) => {
     });
 };
 
-authController.signup = (req, res, next) => {
+authController.signup = async (req, res, next) => {
   const {username, password, email } = req.body
   const signup = 'INSERT INTO users(username, password, email) VALUES($1, $2, $3) RETURNING *'
-  db.query(signup, [username, password, email])
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  db.query(signup, [username, hashedPassword, email])
     .then((data: any) => {
       res.locals.user = data.rows[0]
       return next()
