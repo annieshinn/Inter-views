6fd2dd2f2fd120b63f2100aa2aa381e48aba471b
Annie S <annieshin1997@gmail.com>
2021-03-31 10:35:48 -0700

    cleanin'

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,14 +6,14 @@ const Login = () => {
     <div className="formContainer"> 
       <form> 
         <label>Username:</label> 
-        <input id="username" className="login" type="text" placeholder="Username" /> 
+        <input id="username" className="login" type="text" placeholder="Username" />
 
         <label>Password:</label> 
         <input id="password" className="login" type="password" placeholder="Password" />
 
         <button type="submit" className="login">Login</button> 
         
-        <button id="oauth" type="submit" className="login"> Oauth </button> 
+        <button id="oauth" type="submit" className="login">OAuth</button> 
       </form> 
     </div> 
   )
