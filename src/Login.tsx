1d44062478a3635f54c2bbcbf69cc54714794316
“Gabriel <bielchristo@hotmail.com>
2021-03-30 23:04:55 -0400

    added login component, keys file
    Co-authored-by: annieshinn <annieshinn@users.noreply.github.com>

---
+++ src/Login.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Login = () => {
+
+  return (
+    <div> 
+      <form> 
+        <label> 
+          Username:
+          <input id="username" className="login" type="text" placeholder="Username" /> 
+        </label> 
+        <label> 
+          Password:
+          <input id="password" className="login" type="password" placeholder="Password" /> 
+        </label> 
+        <button type="submit" className="login"> Login </button> 
+        <button id="oauth" type="submit" className="login" > Oauth </button> 
+      </form> 
+    </div> 
+  )
+}
+
+export default Login
