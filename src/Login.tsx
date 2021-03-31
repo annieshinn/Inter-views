a93ebbf716e4e9ea6bf16713e4bdc9ebf464ab07
Annie S <annieshin1997@gmail.com>
2021-03-31 09:41:09 -0700

    reordering form and its css

--- src/Login.tsx
+++ src/Login.tsx
@@ -5,16 +5,15 @@ const Login = () => {
   return (
     <div className="formContainer"> 
       <form> 
-        <label> 
-          Username:
-          <input id="username" className="login" type="text" placeholder="Username" /> 
-        </label> 
-        <label> 
-          Password:
-          <input id="password" className="login" type="password" placeholder="Password" /> 
-        </label> 
-        <button type="submit" className="login"> Login </button> 
-        <button id="oauth" type="submit" className="login" > Oauth </button> 
+        <label>Username:</label> 
+        <input id="username" className="login" type="text" placeholder="Username" /> 
+
+        <label>Password:</label> 
+        <input id="password" className="login" type="password" placeholder="Password" />
+
+        <button type="submit" className="login">Login</button> 
+        
+        <button id="oauth" type="submit" className="login"> Oauth </button> 
       </form> 
     </div> 
   )
