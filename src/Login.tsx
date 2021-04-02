58f1ee4477de0f1baebf92ad2fc0ac70f6785584
Annie S <annieshin1997@gmail.com>
2021-04-01 17:03:49 -0700

    create onchange properties for login page

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,21 +1,64 @@
-import React from "react"
+import React, { SyntheticEvent } from "react"
+import { useDispatch } from 'react-redux';
+import { createActionLogin, createActionChangeUsername, createActionChangePassword } from './slices/authSlice';
+import { useAppSelector } from './types/reduxTypes';
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const usernameValue = useAppSelector((state) => state.authenticate.username)
+  const passwordValue = useAppSelector((state) => state.authenticate.password)
+
+  const login = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    const action = createActionLogin(['user', 'pass']);
+    dispatch(action);
+  }
+
+  const usernameOnChange = (value: string) => {
+    const action = createActionChangeUsername(value);
+    dispatch(action);
+  }
+
+  const passwordOnChange = (value: string) => {
+    const action = createActionChangePassword(value);
+    dispatch(action);
+  }
 
   return (
     <div className="formContainer"> 
       <form> 
         <label>Username:</label> 
-        <input id="username" className="login" type="text" placeholder="Username" />
+        <input
+          id="username"
+          className="login"
+          type="text"
+          placeholder="Username"
+          onChange={(e) => usernameOnChange(e.target.value)}
+        />
 
         <label>Password:</label> 
-        <input id="password" className="login" type="password" placeholder="Password" />
+        <input
+          id="password"
+          className="login"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => passwordOnChange(e.target.value)}
+        />
 
-        <button type="submit" className="login">Login</button> 
+        <button
+          type="submit"
+          className="login"
+          onClick={login}>
+            Login
+        </button> 
         
-        <button id="oauth" type="submit" className="login">OAuth</button>
+        <button id="oauth"
+          type="submit"
+          className="login">
+            OAuth
+        </button>
         <br/>
-
         <a href="/signup">click here to sign up</a>
       </form> 
     </div> 
